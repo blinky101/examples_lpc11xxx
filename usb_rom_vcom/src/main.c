a47664686ff7b6cc435c2e98e5c1675789be63f4
Ingmar Jager <ingmarjager@gmail.com>
2018-11-20 17:37:50 +0100

    Add USB rom api virtual com port example
    This example shows how to set up a cdc device that simply echoes back
    all received commands.

---
+++ usb_rom_vcom/src/main.c
@@ -0,0 +1,221 @@
+#include "board.h"
+#include "board_GPIO_ID.h"
+
+#include <chip.h>
+#include <lpc_tools/boardconfig.h>
+#include <lpc_tools/GPIO_HAL.h>
+#include <lpc_tools/GPIO_HAL_LPC.h>
+#include <mcu_timing/delay.h>
+
+// usbd includes
+#include <string.h>
+#include "usb/app_usbd_cfg.h"
+#include "usb/cdc_vcom.h"
+
+
+static USBD_HANDLE_T g_hUsb;
+static uint8_t g_rxBuff[256];
+USBD_API_T* gUSB_API;
+
+
+
+/* Initialize pin and clocks for USB0/USB1 port */
+static void usb_pin_clk_init(void)
+{
+	/* enable USB main clock */
+	Chip_Clock_SetUSBClockSource(SYSCTL_USBCLKSRC_PLLOUT, 1);
+	/* Enable AHB clock to the USB block and USB RAM. */
+	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_USB);
+	Chip_Clock_EnablePeriphClock(SYSCTL_CLOCK_USBRAM);
+	/* power UP USB Phy */
+	Chip_SYSCTL_PowerUp(SYSCTL_POWERDOWN_USBPAD_PD);
+}
+void USB_IRQHandler(void)
+{
+	gUSB_API->hw->ISR(g_hUsb);
+}
+
+/* Find the address of interface descriptor for given class type. */
+USB_INTERFACE_DESCRIPTOR *find_IntfDesc(const uint8_t *pDesc, uint32_t intfClass)
+{
+	USB_COMMON_DESCRIPTOR *pD;
+	USB_INTERFACE_DESCRIPTOR *pIntfDesc = 0;
+	uint32_t next_desc_adr;
+
+	pD = (USB_COMMON_DESCRIPTOR *) pDesc;
+	next_desc_adr = (uint32_t) pDesc;
+
+	while (pD->bLength) {
+		/* is it interface descriptor */
+		if (pD->bDescriptorType == USB_INTERFACE_DESCRIPTOR_TYPE) {
+
+			pIntfDesc = (USB_INTERFACE_DESCRIPTOR *) pD;
+			/* did we find the right interface descriptor */
+			if (pIntfDesc->bInterfaceClass == intfClass) {
+				break;
+			}
+		}
+		pIntfDesc = 0;
+		next_desc_adr = (uint32_t) pD + pD->bLength;
+		pD = (USB_COMMON_DESCRIPTOR *) next_desc_adr;
+	}
+
+	return pIntfDesc;
+}
+
+
+// TODO it would be nice to add clock_set_frequency() support via lpc_tools
+// instead of this hardcoded frequency
+void SystemSetupClocking(void)
+{
+	volatile int i;
+
+	/* Powerup main oscillator */
+	Chip_SYSCTL_PowerUp(SYSCTL_POWERDOWN_SYSOSC_PD);
+
+	/* Wait 200us for OSC to be stablized, no status
+	   indication, dummy wait. */
+	for (i = 0; i < 0x100; i++) {}
+
+	/* Set system PLL input to main oscillator */
+	Chip_Clock_SetSystemPLLSource(SYSCTL_PLLCLKSRC_MAINOSC);
+
+	/* Power down PLL to change the PLL divider ratio */
+	Chip_SYSCTL_PowerDown(SYSCTL_POWERDOWN_SYSPLL_PD);
+
+	/* Setup PLL for main oscillator rate (FCLKIN = 12MHz) * 4 = 48MHz
+	   MSEL = 3 (this is pre-decremented), PSEL = 1 (for P = 2)
+	   FCLKOUT = FCLKIN * (MSEL + 1) = 12MHz * 4 = 48MHz
+	   FCCO = FCLKOUT * 2 * P = 48MHz * 2 * 2 = 192MHz (within FCCO range) */
+	Chip_Clock_SetupSystemPLL(3, 1);
+
+	/* Powerup system PLL */
+	Chip_SYSCTL_PowerUp(SYSCTL_POWERDOWN_SYSPLL_PD);
+
+	/* Wait for PLL to lock */
+	while (!Chip_Clock_IsSystemPLLLocked()) {}
+
+	/* Set system clock divider to 1 */
+	Chip_Clock_SetSysClockDiv(1);
+
+	/* Setup FLASH access to 3 clocks */
+	Chip_FMC_SetFLASHAccess(FLASHTIM_50MHZ_CPU);
+
+	/* Set main clock source to the system PLL. This will drive 48MHz
+	   for the main clock and 48MHz for the system clock */
+	Chip_Clock_SetMainClockSource(SYSCTL_MAINCLKSRC_PLLOUT);
+
+	/* Set USB PLL input to main oscillator */
+	Chip_Clock_SetUSBPLLSource(SYSCTL_PLLCLKSRC_MAINOSC);
+	/* Setup USB PLL  (FCLKIN = 12MHz) * 4 = 48MHz
+	   MSEL = 3 (this is pre-decremented), PSEL = 1 (for P = 2)
+	   FCLKOUT = FCLKIN * (MSEL + 1) = 12MHz * 4 = 48MHz
+	   FCCO = FCLKOUT * 2 * P = 48MHz * 2 * 2 = 192MHz (within FCCO range) */
+	Chip_Clock_SetupUSBPLL(3, 1);
+
+	/* Powerup USB PLL */
+	Chip_SYSCTL_PowerUp(SYSCTL_POWERDOWN_USBPLL_PD);
+
+	/* Wait for PLL to lock */
+	while (!Chip_Clock_IsUSBPLLLocked()) {}
+}
+
+typedef union  {
+	struct  {
+		int core: 8;
+		int hw: 4;
+		int msc: 4;
+		int dfu: 4;
+		int hid: 4;
+		int cdc: 4;
+		int reserved: 4;
+	} fields;
+	uint32_t raw;
+} usbversion;
+
+int main(void)
+{
+    board_setup();
+    board_setup_NVIC();
+    board_setup_pins();
+
+	// configure System Clock at 48MHz
+	SystemSetupClocking();
+	SystemCoreClockUpdate();
+
+    delay_init();
+
+    // get the GPIO with the led (see board.c)
+    const GPIO *led = board_get_GPIO(GPIO_ID_LED);
+
+
+
+	// USB init
+	gUSB_API = (USBD_API_T*)LPC_ROM_API->usbdApiBase; //0x1FFF1F24
+
+	volatile usbversion v = (usbversion) gUSB_API->version;
+
+	USBD_API_INIT_PARAM_T usb_param;
+	USB_CORE_DESCS_T desc;
+	ErrorCode_t ret = LPC_OK;
+	uint32_t prompt = 0, rdCnt = 0;
+
+/* enable clocks and pinmux */
+	usb_pin_clk_init();
+
+	/* initilize call back structures */
+	memset((void *) &usb_param, 0, sizeof(USBD_API_INIT_PARAM_T));
+	usb_param.usb_reg_base = LPC_USB0_BASE;
+	usb_param.max_num_ep = 3;
+	usb_param.mem_base = USB_STACK_MEM_BASE;
+	usb_param.mem_size = USB_STACK_MEM_SIZE;
+
+	/* Set the USB descriptors */
+	desc.device_desc = (uint8_t *) &USB_DeviceDescriptor[0];
+	desc.string_desc = (uint8_t *) &USB_StringDescriptor[0];
+	/* Note, to pass USBCV test full-speed only devices should have both
+	   descriptor arrays point to same location and device_qualifier set to 0.
+	 */
+	desc.high_speed_desc = (uint8_t *) &USB_FsConfigDescriptor[0];
+	desc.full_speed_desc = (uint8_t *) &USB_FsConfigDescriptor[0];
+	desc.device_qualifier = 0;
+
+	/* USB Initialization */
+	ret = gUSB_API->hw->Init(&g_hUsb, &desc, &usb_param);
+	if (ret == LPC_OK) {
+
+		/* Init VCOM interface */
+		ret = vcom_init(g_hUsb, &desc, &usb_param);
+		if (ret == LPC_OK) {
+			/*  enable USB interrrupts */
+			NVIC_EnableIRQ(USB0_IRQn);
+			/* now connect */
+			gUSB_API->hw->Connect(g_hUsb, 1);
+		}
+
+	}
+
+	while(true)
+	{
+
+		/* Check if host has connected and opened the VCOM port */
+		if ((vcom_connected() != 0) && (prompt == 0)) {
+			// vcom_write("Hello World!!\r\n", 15);
+			vcom_write("Jitter VCOM Example.\r\n", 22);
+			prompt = 1;
+		}
+		/* If VCOM port is opened echo whatever we receive back to host. */
+		if (prompt) {
+			rdCnt = vcom_bread(&g_rxBuff[0], 256);
+			if (rdCnt) {
+				g_rxBuff[0] = 'x';
+				vcom_write(&g_rxBuff[0], rdCnt);
+				GPIO_HAL_toggle(led);
+			}
+		}
+		/* Sleep until next IRQ happens */
+		__WFI();
+	}
+	return 0;
+}
+
