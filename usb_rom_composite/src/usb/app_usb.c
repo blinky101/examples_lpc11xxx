1f7412eaad879fe292a8359a47515d3558d35e2e
Sjors de Wit <dev@sjorsdewit.name>
2018-12-30 16:37:04 +0100

    USB: add workaround for some rom driver bugs
    - usb_rom_composite: fixed
    - usb_rom_vcom: fixed
    - usb_rom_msc: fixed
    
    Without this fix, enumeration is very flaky (especially on linux?).
    With these workarounds, enumeration seems to be very stable and without errors.
    Some more testing on different platforms may be required though..
    
    The fixes are based on example code and hints from multiple errata sheets
    for the LPC11Uxx and similar platforms.

--- usb_rom_composite/src/usb/app_usb.c
+++ usb_rom_composite/src/usb/app_usb.c
@@ -19,9 +19,72 @@ USBD_API_T* gUSB_API;
 
 void USB_IRQHandler(void)
 {
+    uint32_t *addr = (uint32_t *) LPC_USB->EPLISTSTART;
+    // WORKAROUND for artf32289 ROM driver BUG:
+    // As part of USB specification the device should respond
+    // with STALL condition for any unsupported setup packet. The host will send
+    // new setup packet/request on seeing STALL condition for EP0 instead of sending
+    // a clear STALL request. Current driver in ROM doesn't clear the STALL
+    // condition on new setup packet which should be fixed.
+
+    if ( LPC_USB->DEVCMDSTAT & _BIT(8) ) {	// if setup packet is received
+        addr[0] &= ~(_BIT(29));	// clear EP0_OUT stall
+        addr[2] &= ~(_BIT(29));	// clear EP0_IN stall
+    }
+
 	gUSB_API->hw->ISR(g_hUsb);
 }
 
+
+// Workaround for USB_ROM.1 bug
+typedef volatile struct _EP_LIST {
+    uint32_t  buf_ptr;
+    uint32_t  buf_length;
+} EP_LIST;
+ErrorCode_t workaround_stall(USBD_HANDLE_T hUsb)
+{
+    ErrorCode_t ret = LPC_OK;
+    USB_CORE_CTRL_T *pCtrl = (USB_CORE_CTRL_T *) hUsb;
+    EP_LIST      *epQueue;
+    int32_t      i;
+    /*    WORKAROUND for Case 2:
+          Code clearing STALL bits in endpoint reset routine corrupts memory area
+          next to the endpoint control data.
+          */
+    if (pCtrl->ep_halt != 0) { /* check if STALL is set for any endpoint */
+        /* get pointer to HW EP queue */
+        epQueue = (EP_LIST *) LPC_USB->EPLISTSTART;
+        /* check if the HW STALL bit for the endpoint is cleared due to bug. */
+        for (i = 1; i < pCtrl->max_num_ep; i++) {
+            /* check OUT EPs */
+            if ( pCtrl->ep_halt & (1 << i)) {
+                /* Check if HW EP queue also has STALL bit = _BIT(29) is set */
+                if (( epQueue[i << 1].buf_ptr & _BIT(29)) == 0) {
+                    /* bit not set, cleared by BUG. So set it back. */
+                    epQueue[i << 1].buf_ptr |= _BIT(29);
+                }
+            }
+            /* Check IN EPs */
+            if ( pCtrl->ep_halt & (1 << (i + 16))) {
+                /* Check if HW EP queue also has STALL bit = _BIT(29) is set */
+                if (( epQueue[(i << 1) + 1].buf_ptr & _BIT(29)) == 0) {
+                    /* bit not set, cleared by BUG. So set it back. */
+                    epQueue[(i << 1) + 1].buf_ptr |= _BIT(29);
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+// workaround USB_ROM.3
+void  *g_pMscCtrl;
+ErrorCode_t workaround_mwMSC_Reset(USBD_HANDLE_T hUsb)
+{
+    ((USB_MSC_CTRL_T *)g_pMscCtrl)->CSW.dSignature = 0;
+    ((USB_MSC_CTRL_T *)g_pMscCtrl)->BulkStage = 0;
+    return LPC_OK;
+}
 
 bool app_usb_init()
 {
@@ -36,7 +99,18 @@ bool app_usb_init()
 	/* initilize call back structures */
 	memset((void *) &usb_param, 0, sizeof(USBD_API_INIT_PARAM_T));
 	usb_param.usb_reg_base = LPC_USB0_BASE;
-	usb_param.max_num_ep = USB_MAX_EP_NUM;
+
+    // See errata USB_ROM.1: max_num_ep should be at least n+1, where n
+    // is the amount of endpoints in use (ep1_in + ep1_out = 2).
+	//usb_param.max_num_ep = USB_MAX_EP_NUM+1;
+    
+    // NOTE: USB_MAX_EP_NUM is not guaranteed to be enough: up to 10 enpoints
+    // can be active (ep1-5 in + ep1-5 out)
+	usb_param.max_num_ep = USB_MAX_EP_NUM + 1;
+
+    usb_param.USB_Interface_Event = workaround_stall;       // See errata USB_ROM.1
+    usb_param.USB_Reset_Event = workaround_mwMSC_Reset;     // See errata USB_ROM.3
+
 	usb_param.mem_base = USB_STACK_MEM_BASE;
 	usb_param.mem_size = USB_STACK_MEM_SIZE;
 
@@ -73,6 +147,10 @@ bool app_usb_init()
 				case USB_DEVICE_CLASS_STORAGE:
 					usb_param.mem_base = MSC_PARAM_MEM;
 					usb_param.mem_size = MSC_RARAM_SIZE;
+
+                    // workaround USB_ROM.3
+                    g_pMscCtrl = (void *)(usb_param.mem_base);
+
 					ret = usb_msc_mem_init(g_hUsb, pIntfDesc, &usb_param.mem_base, &usb_param.mem_size);
 
 					break;
--- usb_rom_msc/src/main.c
+++ usb_rom_msc/src/main.c
@@ -19,7 +19,7 @@
 static USBD_HANDLE_T g_hUsb;
 USBD_API_T *gUSB_API;
 
-GPIO *led;
+const GPIO *led;
 
 /* Mass Storage Memory Layout */
 #define MSC_BlockSize 512
@@ -163,7 +163,71 @@ static void usb_pin_clk_init(void)
 void USB_IRQHandler(void)
 {
 	GPIO_HAL_toggle(led);
+
+
+    uint32_t *addr = (uint32_t *) LPC_USB->EPLISTSTART;
+    // WORKAROUND for artf32289 ROM driver BUG:
+    // As part of USB specification the device should respond
+    // with STALL condition for any unsupported setup packet. The host will send
+    // new setup packet/request on seeing STALL condition for EP0 instead of sending
+    // a clear STALL request. Current driver in ROM doesn't clear the STALL
+    // condition on new setup packet which should be fixed.
+
+    if ( LPC_USB->DEVCMDSTAT & _BIT(8) ) {	// if setup packet is received
+        addr[0] &= ~(_BIT(29));	// clear EP0_OUT stall
+        addr[2] &= ~(_BIT(29));	// clear EP0_IN stall
+    }
 	gUSB_API->hw->ISR(g_hUsb);
+}
+
+// Workaround for USB_ROM.1 bug
+typedef volatile struct _EP_LIST {
+    uint32_t  buf_ptr;
+    uint32_t  buf_length;
+} EP_LIST;
+ErrorCode_t workaround_stall(USBD_HANDLE_T hUsb)
+{
+    ErrorCode_t ret = LPC_OK;
+    USB_CORE_CTRL_T *pCtrl = (USB_CORE_CTRL_T *) hUsb;
+    EP_LIST      *epQueue;
+    int32_t      i;
+    /*    WORKAROUND for Case 2:
+          Code clearing STALL bits in endpoint reset routine corrupts memory area
+          next to the endpoint control data.
+          */
+    if (pCtrl->ep_halt != 0) { /* check if STALL is set for any endpoint */
+        /* get pointer to HW EP queue */
+        epQueue = (EP_LIST *) LPC_USB->EPLISTSTART;
+        /* check if the HW STALL bit for the endpoint is cleared due to bug. */
+        for (i = 1; i < pCtrl->max_num_ep; i++) {
+            /* check OUT EPs */
+            if ( pCtrl->ep_halt & (1 << i)) {
+                /* Check if HW EP queue also has STALL bit = _BIT(29) is set */
+                if (( epQueue[i << 1].buf_ptr & _BIT(29)) == 0) {
+                    /* bit not set, cleared by BUG. So set it back. */
+                    epQueue[i << 1].buf_ptr |= _BIT(29);
+                }
+            }
+            /* Check IN EPs */
+            if ( pCtrl->ep_halt & (1 << (i + 16))) {
+                /* Check if HW EP queue also has STALL bit = _BIT(29) is set */
+                if (( epQueue[(i << 1) + 1].buf_ptr & _BIT(29)) == 0) {
+                    /* bit not set, cleared by BUG. So set it back. */
+                    epQueue[(i << 1) + 1].buf_ptr |= _BIT(29);
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+// workaround USB_ROM.3
+void  *g_pMscCtrl;
+ErrorCode_t workaround_mwMSC_Reset(USBD_HANDLE_T hUsb)
+{
+    ((USB_MSC_CTRL_T *)g_pMscCtrl)->CSW.dSignature = 0;
+    ((USB_MSC_CTRL_T *)g_pMscCtrl)->BulkStage = 0;
+    return LPC_OK;
 }
 
 void SystemSetupClocking(void)
@@ -217,9 +281,16 @@ int main(void)
 	/* initilize call back structures */
 	memset((void *)&usb_param, 0, sizeof(USBD_API_INIT_PARAM_T));
 	usb_param.usb_reg_base = LPC_USB0_BASE;
+
+    // See errata USB_ROM.1: max_num_ep should be at least n+1, where n
+    // is the amount of endpoints in use (ep1_in + ep1_out = 2).
+	usb_param.max_num_ep = 2 + 1;
+
+    usb_param.USB_Interface_Event = workaround_stall;       // See errata USB_ROM.1
+    usb_param.USB_Reset_Event = workaround_mwMSC_Reset;     // See errata USB_ROM.3
+
 	usb_param.mem_base = MSC_PARAM_MEM;
 	usb_param.mem_size = MSC_RARAM_SIZE;
-	usb_param.max_num_ep = 2;
 
 	USB_CORE_DESCS_T desc;
 
@@ -236,6 +307,10 @@ int main(void)
 	{
 		usb_param.mem_base = 0x10001500;
 		usb_param.mem_size = 0x300;
+
+        // workaround USB_ROM.3
+        g_pMscCtrl = (void *)(usb_param.mem_base);
+
 		ret = usb_msc_mem_init(g_hUsb,
 							   (USB_INTERFACE_DESCRIPTOR *)&USB_FsConfigDescriptor[sizeof(USB_CONFIGURATION_DESCRIPTOR)],
 							   &usb_param.mem_base, &usb_param.mem_size);
